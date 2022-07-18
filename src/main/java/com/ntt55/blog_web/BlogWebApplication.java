43081c7bc9a0da2059667b1be4e4143c3d281ae0
thongnt55 <109049076+thongnt55@users.noreply.github.com>
2022-07-18 10:26:14 +0700

    first commit

---
+++ src/main/java/com/ntt55/blog_web/BlogWebApplication.java
@@ -0,0 +1,13 @@
+package com.ntt55.blog_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogWebApplication.class, args);
+    }
+
+}
